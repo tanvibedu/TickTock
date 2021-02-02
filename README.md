0bfda00cb160a3dba2eb82b0115b7a55d45d6a68
tanvibedu <76459290+tanvibedu@users.noreply.github.com>
2021-02-01 22:02:45 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TickTock
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,58 @@
+function setup() {
+  createCanvas(800,400);
+  
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background("black");  
+  translate(200,200);
+  rotate(-90);
+  
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  scAngle = map(sc, 0, 60, 0, 360); 
+  mnAngle = map(mn,0,60,0,360); 
+  hrAngle = map(hr % 12,0,12,0,360);
+  push(); 
+  rotate(scAngle); 
+  //rotate the hand based on angle calculated 
+  stroke(255,0,0);
+  strokeWeight(7); 
+  line(0,0,100,0); 
+  pop();
+
+  push(); 
+  rotate(mnAngle); 
+  //rotate the hand based on angle calculated 
+  stroke(0, 255,0);
+  strokeWeight(7); 
+  line(0,0,75, 0);
+  pop();
+
+  push(); 
+  rotate(hrAngle); 
+  //rotate the hand based on angle calculated 
+  stroke(0,0, 255); 
+  strokeWeight(7); 
+  line(0,0,50,0); 
+  pop();
+  stroke(255,0,255);
+  point(0,0);
+  strokeWeight(10);
+  noFill();
+  stroke(255,0,0); 
+  arc(0,0,300,300,0,scAngle);
+  stroke(0, 255, 0); 
+  arc(0,0,280,280,0,mnAngle);
+  stroke(0,0, 255); 
+  arc(0,0,260,260,0,hrAngle);
+
+  drawSprites();
+
+
+ 
+}
+
